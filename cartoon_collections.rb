20651f18144f203d637a24c1fd57cb632f496c76
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 17:33:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,14 @@ def long_planeteer_calls(calls)# code an argument here
       
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(input)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  input.each do |look|
+    if cheese_types.include?(look)
+      p look
+    else
+      return nil
+    end
+  end
 end
