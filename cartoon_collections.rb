4538541bfe8e182f788c136bcc2764fd2f187b66
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 17:39:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_the_cheese(input)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   input.each do |look|
-    #binding.pry
+    binding.pry
     if cheese_types.include?(look)
       return input[look]
     else
