6da24b66457fa5f67b31979926ddc261670c304e
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 17:02:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(names)# code an argument here
   # Your code here
-  names.map{ |x| p names[x].capitalize +"!"}
+  names.collect{ |x| p names[x].capitalize +"!"}
   
 end
 
