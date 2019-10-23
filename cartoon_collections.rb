04b6202fcefa1bc8525835b43a88fc45960cb64e
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 16:46:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ require 'pry'
 def roll_call_dwarves(names)# code an argument here
   # Your code here
   binding.pry
-  names.collect {|x| p x}
+  #names.collect {|x| p x}
 end
 
 def summon_captain_planet# code an argument here
