705aa2dc95f811521b4571ce95d3c0b3d9b8968c
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 17:23:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,13 +12,8 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.map{ |x|
-    if x.size <= 4
-      return false
-    else
-      return true
-    end}
-  end
+  calls.map{ |x| return false unless x.length > 4}
+      
 end
 
 def find_the_cheese# code an argument here
